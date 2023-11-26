a74161cc2d7038ca8d4b90d656cd4a183e2a7f4c
Sundaram pandey <thisnewcomputerismine2020@gmail.com>
2023-11-27 01:09:44 +0530

    Project complete

---
+++ README.md
@@ -0,0 +1,4 @@
+# Nike Clone
+
+This is a simple Nike clone site with just the landing page for now.
+
---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,35 @@
+const ShoeCard = ({imgURL,changeBigShoeImg,bigShoeImg}) => {
+     
+    
+    const handleClick=()=>{
+        if(bigShoeImg!== imgURL.bigShoe){  //if bigshoe image is not already selected then it will call the function changebigshoe,which will call the setbigshoe for state variable
+            changeBigShoeImg(imgURL.bigShoe)
+        }
+    }   
+
+
+
+  return (
+    <div className={`border-2 rounded-xl cursor-pointer max-sm:flex-1
+        ${bigShoeImg=== imgURL.bigShoe ?'border-coral-red' : 'border-transparent'}
+        
+        
+    
+    `}
+
+    onClick={handleClick}
+    >
+        <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+            <img src={imgURL.thumbnail} alt="shoe-collection"
+            width={127}
+            height={103}
+            className="object-contain"
+            />
+        </div>
+    </div>
+  )
+}
+
+export default ShoeCard
+
+
